6b317f05fcbb9d4682e5d5d59a9413223bb7bef1
Tex Liao <linex1008@gmail.com>
2021-02-24 00:28:28 +0800

    Update tex homework

--- src/Homework/Tex/components/SongTable.js
+++ src/Homework/Tex/components/SongTable.js
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+function millisToMinutesAndSeconds(millis) {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
+
 const SongTable = ({ songData }) => {
   const [select, setSelect] = useState('');
   return (
@@ -7,6 +13,8 @@ const SongTable = ({ songData }) => {
       {songData.map((songItem, index) => {
         const id = songItem.track.id;
         const isSelect = id === select;
+        const artist = songItem.track.album.artists[0];
+        const album = songItem.track.album;
         return (
           <div
             className={`song-table-list ${isSelect ? 'select' : ''}`}
@@ -17,19 +25,19 @@ const SongTable = ({ songData }) => {
           >
             <div className="song-table-list-index">{index + 1}</div>
             <div className="song-table-list-title">
-              <img
-                src={songItem.track.album.images[1].url}
-                alt="album-cover"
-              ></img>
+              <img src={album.images[1].url} alt="album-cover" />
               <div className="song-desc">
                 <div className="song-desc-name">{songItem.track.name}</div>
                 <div className="song-desc-artist">
-                  {songItem.track.album.artists[0].name}
+                  <a href={artist.external_urls.spotify}>{artist.name}</a>
                 </div>
               </div>
             </div>
             <div className="song-table-list-album">
-              {songItem.track.album.name}
+              <a href={album.external_urls.spotify}>{album.name}</a>
+            </div>
+            <div className="song-table-list-duration">
+              {millisToMinutesAndSeconds(songItem.track.duration_ms)}
             </div>
           </div>
         );
